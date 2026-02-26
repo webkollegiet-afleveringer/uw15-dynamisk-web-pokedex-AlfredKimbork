7402e6be58fd6f3ae2bf651b369848f4b7c33322
Alfred K. <107874027+AlfredKimbork@users.noreply.github.com>
2026-02-26 10:58:41 +0100

    loads pokemons with pics as you scroll down

--- js/script.js
+++ js/script.js
@@ -1,17 +1,32 @@
-async function getCharacters() {
-    const data = await fetch('https://pokeapi.co/api/v2/pokemon')
+async function getCharacters(page) {
+    const data = await fetch(page)
         .then(response => response.json())
-
-    console.log(data)
+        console.log(data)
+    return data
 }
 
-async function getCharacter(characterURL) {
-    const data = await fetch(`${characterURL}`)
-        .then(response => response.json())
+async function loadMore(page = "https://pokeapi.co/api/v2/pokemon") {
+    const data = await getCharacters(page)
+    const characters = data.results
+    const observer = new IntersectionObserver(entries => {
+        if(entries[0].isIntersecting) {
+            loadMore(data.next)
+            observer.unobserve(entries[0].target)
+        }
+    }, {
+        rootMargin: "0 0 1000px 0"
+    })
 
-    console.log(data)
+    document.querySelector(".poke-list").innerHTML += characters.map(character => { return `    
+        <li class="poke-card">
+            ${character.name}
+                <img class="card-img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${character.url.slice(0, -1).split("/").pop(-1)}.png" alt="${character.name}">
+        </li>`}).join(" ")
+
+    const lastCard = document.querySelector(".poke-card:last-child");
+    observer.observe(lastCard)
 }
 
+document.querySelector("#main").innerHTML = `<ol class="poke-list"></ol>`;
 
-
-getCharacters()
+loadMore()

730d2189effb50186d8b5f785f917029fd146dad
Alfred K. <107874027+AlfredKimbork@users.noreply.github.com>
2026-02-27 08:48:59 +0100

    loading bug fixed (got last element on li instead of anchor)

--- js/home.js
+++ js/home.js
@@ -1,36 +1,41 @@
 import { useFetch } from "./useFetch.js";
 
 async function loadMore(page = "https://pokeapi.co/api/v2/pokemon") {
-    const data = await useFetch(page)
-    const characters = data.results
-    const observer = new IntersectionObserver(entries => {
-        if(entries[0].isIntersecting) {
+    const data = await useFetch(page);
+    const characters = data.results;
+    const observer = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
             loadMore(data.next);
             observer.unobserve(entries[0].target);
         }
-    }, { rootMargin: "0 0 100px 0" })
+    }, {
+        rootMargin: "0 0 1000px 0",
+    });
 
-    document.querySelector(".poke-list").innerHTML += characters.map(character => { 
-        const id = character.url.slice(0, -1).split("/").pop(-1)
-        let idFormat
-        if(id.length <= 4) {
+document.querySelector(".poke-list").innerHTML += characters
+    .map((character) => {
+        const id = character.url.slice(0, -1).split("/").pop(-1);
+        let idFormat;
+
+        if (id.length <= 4) {
             switch (id.length) {
                 case 1:
-                    idFormat = `#000${id}`
+                    idFormat = `#000${id}`;
                     break;
                 case 2:
-                    idFormat = `#00${id}`
+                    idFormat = `#00${id}`;
                     break;
                 case 3:
-                    idFormat = `#0${id}`
+                    idFormat = `#0${id}`;
                     break;
                 case 4:
-                    idFormat = `#${id}`
+                    idFormat = `#${id}`;
                     break;
                 default:
-                    idFormat = `#????`
+                    idFormat = `#????`;
                     break;
             }
+
             return `    
                 <li>
                     <a class="poke-card" href="card.html?id=${id}">
@@ -39,18 +44,13 @@ async function loadMore(page = "https://pokeapi.co/api/v2/pokemon") {
                         ${character.name}
                     </a>
                 </li>
-            `
+            `;
         }
-    }).join(" ")
+    }).join(" ");
 
-        document.querySelectorAll(".poke-card").forEach(card => card.addEventListener("click", event => {
-            getCharacters(`https://pokeapi.co/api/v2/pokemon/${event.currentTarget.id}/`)
-        }))
-
-        
-        observer.observe(document.querySelector(".poke-card:last-child"))
-    }
+    observer.observe(document.querySelector("li:last-child"));
+}
 
 document.querySelector("#main").innerHTML = `<ul class="poke-list"></ul>`;
 
-loadMore()
+loadMore();

8ec51bf46ffdb8d1b707c1df6acd2ddd50e86dae
Alfred K. <107874027+AlfredKimbork@users.noreply.github.com>
2026-03-04 13:45:12 +0100

    card styling and fixed

--- js/useFormat.js
+++ js/useFormat.js
@@ -1,10 +1,11 @@
 export const useFormat = character => {
-    const id = character.url.slice(0, -1).split("/").pop(-1);
-    const page = character.url.slice(0, -3).split("/").pop(-1);
+    let id = character.url.slice(0, -1).split("/").pop(-1);
+    let page = character.url.slice(0, -3).split("/").pop(-1);
+    if(page == "") page = "people"
 
     return `    
         <li>
-            <a class="__card" href="card.html?page=${page}&id=${id}">
+            <a class="__card --hollow" href="card.html?page=${page}&id=${id}">
                 <img class="card-img" src="../assets/img/${id}.jpg" alt="${character.name}">
                 ${character.name}
             </a>

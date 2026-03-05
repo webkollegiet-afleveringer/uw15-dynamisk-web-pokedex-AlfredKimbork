9f999ef56e6c5a33951a46508f14478fa90cdd47
Alfred K. <107874027+AlfredKimbork@users.noreply.github.com>
2026-03-05 12:59:58 +0100

    page control fixes

--- js/home.js
+++ js/home.js
@@ -12,6 +12,7 @@ let list
 if(page === null) page = "people"
 
 async function loadMore(url = ``) {
+    console.log("triggered")
     const data = await useFetch(url);
     const characters = data.results;
     mainDOM.innerHTML = `<ul class="__list"></ul>`;
@@ -22,22 +23,14 @@ async function loadMore(url = ``) {
             .map(character => {
                 return formatList(character, page)
             }).join(" ");
-    
-    } else {
-        characters.sort((a, b) => a.name.localeCompare(b.name))
-            for (let i = (currentPage * batchSize) - batchSize; i < currentPage * batchSize; i++) {
-                list.innerHTML += formatList(characters[i], page);
-                
-                
-            }
     }
 
     mainDOM.innerHTML += `
         <nav class="page-nav">
-            <button class="prev-bt --btn --clear" id="prevBtn">
-                <svg width="15" height="20" viewBox="0 0 15 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <button class="prev-btn --btn --clear" id="prevBtn">
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <g filter="url(#filter0_d_1044_208)">
-                        <path d="M9.175 14.2L4.225 9.25C4.14167 9.16667 4.08333 9.08333 4.05 9C4.01667 8.91667 4 8.825 4 8.725C4 8.625 4.01667 8.53333 4.05 8.45C4.08333 8.36667 4.14167 8.28333 4.225 8.2L9.2 3.225C9.35 3.075 9.52917 3 9.7375 3C9.94583 3 10.125 3.075 10.275 3.225C10.425 3.375 10.4958 3.55833 10.4875 3.775C10.4792 3.99167 10.4 4.175 10.25 4.325L5.85 8.725L10.275 13.15C10.425 13.3 10.5 13.475 10.5 13.675C10.5 13.875 10.425 14.05 10.275 14.2C10.125 14.35 9.94167 14.425 9.725 14.425C9.50833 14.425 9.325 14.35 9.175 14.2Z" fill="white"/>
+                        <path d="M9.175 14.2L4.225 9.25C4.14167 9.16667 4.08333 9.08333 4.05 9C4.01667 8.91667 4 8.825 4 8.725C4 8.625 4.01667 8.53333 4.05 8.45C4.08333 8.36667 4.14167 8.28333 4.225 8.2L9.2 3.225C9.35 3.075 9.52917 3 9.7375 3C9.94583 3 10.125 3.075 10.275 3.225C10.425 3.375 10.4958 3.55833 10.4875 3.775C10.4792 3.99167 10.4 4.175 10.25 4.325L5.85 8.725L10.275 13.15C10.425 13.3 10.5 13.475 10.5 13.675C10.5 13.875 10.425 14.05 10.275 14.2C10.125 14.35 9.94167 14.425 9.725 14.425C9.50833 14.425 9.325 14.35 9.175 14.2Z" fill="#171717"/>
                     </g>
                     <defs>
                         <filter id="filter0_d_1044_208" x="0" y="0" width="14.5" height="19.425" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
@@ -54,20 +47,20 @@ async function loadMore(url = ``) {
                     </defs>
                 </svg>
             </button>
-            <span class="page --white">${currentPage} / ${Math.ceil(data.count / 10)}</span>
+            <span class="page">${currentPage} / ${Math.ceil(data.count / 10)}</span>
             <button class="next-btn --btn --clear" id="nextBtn">
-                <svg width="15" height="20" viewBox="0 0 15 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <g filter="url(#filter0_d_1044_467)">
-                        <path d="M4.21324 14.2131C4.0799 14.0464 4.00907 13.8631 4.00074 13.6631C3.9924 13.4631 4.06324 13.2881 4.21324 13.1381L8.61324 8.7381L4.18824 4.3131C4.0549 4.17976 3.9924 4.00059 4.00074 3.77559C4.00907 3.55059 4.0799 3.37143 4.21324 3.23809C4.3799 3.07143 4.55907 2.99226 4.75074 3.0006C4.9424 3.00893 5.11324 3.08809 5.26324 3.23809L10.2382 8.21309C10.3216 8.29643 10.3799 8.37976 10.4132 8.46309C10.4466 8.54643 10.4632 8.6381 10.4632 8.7381C10.4632 8.8381 10.4466 8.92976 10.4132 9.01309C10.3799 9.09643 10.3216 9.17976 10.2382 9.26309L5.28824 14.2131C5.13824 14.3631 4.96324 14.4339 4.76324 14.4256C4.56324 14.4173 4.3799 14.3464 4.21324 14.2131Z" fill="white"/>
+                        <path d="M4.21324 14.2131C4.0799 14.0464 4.00907 13.8631 4.00074 13.6631C3.9924 13.4631 4.06324 13.2881 4.21324 13.1381L8.61324 8.7381L4.18824 4.3131C4.0549 4.17976 3.9924 4.00059 4.00074 3.77559C4.00907 3.55059 4.0799 3.37143 4.21324 3.23809C4.3799 3.07143 4.55907 2.99226 4.75074 3.0006C4.9424 3.00893 5.11324 3.08809 5.26324 3.23809L10.2382 8.21309C10.3216 8.29643 10.3799 8.37976 10.4132 8.46309C10.4466 8.54643 10.4632 8.6381 10.4632 8.7381C10.4632 8.8381 10.4466 8.92976 10.4132 9.01309C10.3799 9.09643 10.3216 9.17976 10.2382 9.26309L5.28824 14.2131C5.13824 14.3631 4.96324 14.4339 4.76324 14.4256C4.56324 14.4173 4.3799 14.3464 4.21324 14.2131Z" fill="#171717"/>
                     </g>
                     <defs>
                         <filter id="filter0_d_1044_467" x="0" y="0" width="14.4631" height="19.4263" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
                             <feFlood flood-opacity="0" result="BackgroundImageFix"/>
                             <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
                             <feMorphology radius="1" operator="dilate" in="SourceAlpha" result="effect1_dropShadow_1044_467"/>
-                            <feOffset dy="1"/>
                             <feGaussianBlur stdDeviation="1.5"/>
                             <feComposite in2="hardAlpha" operator="out"/>
+                            <feOffset dy="1"/>
                             <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0"/>
                             <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1044_467"/>
                             <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1044_467" result="shape"/>
@@ -77,19 +70,19 @@ async function loadMore(url = ``) {
             </button>
         </nav>
     `;
-    document.querySelector("#prevBtn").addEventListener("click", () => {
-        if(currentPage > 1) {
+    if(currentPage > 1) {
+        document.querySelector("#prevBtn").addEventListener("click", () => {
             currentPage--
             loadMore(data.previous);
-        }
-    });
+        });
+    } else document.querySelector("#prevBtn").classList.add("--off")
 
-    document.querySelector("#nextBtn").addEventListener("click", () => {
-        if(currentPage < Math.ceil(data.count / 10)) {
+    if(currentPage < Math.ceil(data.count / 10)) {
+        document.querySelector("#nextBtn").addEventListener("click", () => {
             currentPage++
             loadMore(data.next);
-        }
-    });
+        });
+    } else document.querySelector("#nextBtn").classList.add("--off")
 }
 
 headerDOM.innerHTML = `
@@ -117,9 +110,7 @@ document.querySelector("#search").addEventListener("input", async event => {
 
     if(event.target.value.length !== 0) {
         list.innerHTML = "";
-        const searchedData = await useFetch(`https://swapi.dev/api/${page}/?search=${event.target.value}&format=json`);
-        console.log(searchedData)
-        searchedData.results.map(result => list.innerHTML += formatList(result, page));
+        loadMore(`https://swapi.dev/api/${page}/?search=${event.target.value}&format=json`)
     } else {
         list.innerHTML = "";
         if (page) loadMore(`https://swapi.dev/api/${page}/?format=json`);
